5e94e5de4e12d1f2470e73c8a9fadaa2b8dc2cbe
Jojy Varghese <jojy@mesosphere.io>
2015-09-25 14:21:32 -0700

    Added docker registry RemotePuller
    First iteration with basic functionality.
    
    Review: https://reviews.apache.org/r/38580

--- src/slave/containerizer/provisioner/docker/local_puller.hpp
+++ src/slave/containerizer/provisioner/docker/local_puller.hpp
@@ -49,7 +49,9 @@ public:
 
   process::Future<std::list<std::pair<std::string, std::string>>> pull(
       const Image::Name& name,
-      const std::string& directory);
+      const Path& directory,
+      const Option<Duration>& timeout,
+      const Option<size_t>& maxSize);
 
 private:
   LocalPuller& operator=(const LocalPuller&) = delete; // Not assignable.
