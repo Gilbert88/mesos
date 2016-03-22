a3d28b5f7bca311b50dde91c0341044fc5fe79a2
Gilbert Song <songzihao1990@gmail.com>
2016-03-22 14:16:15 -0700

    Added test for mounting host system config files.
    Review: https://reviews.apache.org/r/45184

--- src/tests/containerizer/runtime_isolator_tests.cpp
+++ src/tests/containerizer/runtime_isolator_tests.cpp
@@ -317,6 +317,84 @@ TEST_F(DockerRuntimeIsolatorTest,
 }
 
 
+// This test verifies that the missing system configuration files are
+// mounted from the host into the container correctly.
+TEST_F(DockerRuntimeIsolatorTest, ROOT_INTERNET_CURL_MountSystemConfiguration)
+{
+  Try<Owned<cluster::Master>> master = StartMaster();
+  ASSERT_SOME(master);
+
+  slave::Flags flags = CreateSlaveFlags();
+  flags.isolation = "docker/runtime,filesystem/linux";
+  flags.image_providers = "docker";
+  flags.docker_registry = "https://registry-1.docker.io";
+
+  Owned<MasterDetector> detector = master.get()->createDetector();
+  Try<Owned<cluster::Slave>> slave = StartSlave(detector.get(), flags);
+  ASSERT_SOME(slave);
+
+  MockScheduler sched;
+  MesosSchedulerDriver driver(
+      &sched, DEFAULT_FRAMEWORK_INFO, master.get()->pid, DEFAULT_CREDENTIAL);
+
+  EXPECT_CALL(sched, registered(&driver, _, _));
+
+  Future<vector<Offer>> offers;
+  EXPECT_CALL(sched, resourceOffers(&driver, _))
+    .WillOnce(FutureArg<1>(&offers))
+    .WillRepeatedly(Return()); // Ignore subsequent offers.
+
+  driver.start();
+
+  AWAIT_READY(offers);
+  ASSERT_EQ(1u, offers->size());
+
+  const Offer& offer = offers.get()[0];
+
+  // The system config file /etc/resolv.conf does not exist in
+  // alpine's rootfs, so we use it to test mounting system config.
+  // We expect TASK_FINISHED if this file can be successfully
+  // mounted from the host into the container.
+  CommandInfo command;
+  command.set_shell(false);
+  command.set_value("/bin/cat");
+  command.add_arguments("cat");
+  command.add_arguments("/etc/resolv.conf");
+
+  TaskInfo task = createTask(
+      offer.slave_id(),
+      Resources::parse("cpus:1;mem:128").get(),
+      command);
+
+  Image image;
+  image.set_type(Image::DOCKER);
+  image.mutable_docker()->set_name("alpine");
+
+  ContainerInfo* container = task.mutable_container();
+  container->set_type(ContainerInfo::MESOS);
+  container->mutable_mesos()->mutable_image()->CopyFrom(image);
+
+  Future<TaskStatus> statusRunning;
+  Future<TaskStatus> statusFinished;
+  EXPECT_CALL(sched, statusUpdate(&driver, _))
+    .WillOnce(FutureArg<1>(&statusRunning))
+    .WillOnce(FutureArg<1>(&statusFinished));
+
+  driver.launchTasks(offer.id(), {task});
+
+  AWAIT_READY_FOR(statusRunning, Seconds(60));
+  EXPECT_EQ(task.task_id(), statusRunning->task_id());
+  EXPECT_EQ(TASK_RUNNING, statusRunning->state());
+
+  AWAIT_READY(statusFinished);
+  EXPECT_EQ(task.task_id(), statusFinished->task_id());
+  EXPECT_EQ(TASK_FINISHED, statusFinished->state());
+
+  driver.stop();
+  driver.join();
+}
+
+
 #endif // __linux__
 
 } // namespace tests {
