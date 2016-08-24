ad11ca73b0a74a3ba788ba14380e417e34673224
Gilbert Song <songzihao1990@gmail.com>
2016-08-24 13:37:35 -0700

    Added unit test for provisioner recursive listContainers().
    Review: https://reviews.apache.org/r/51393

--- src/tests/containerizer/provisioner_paths_tests.cpp
+++ src/tests/containerizer/provisioner_paths_tests.cpp
@@ -17,6 +17,7 @@
 #include <process/gtest.hpp>
 
 #include <stout/gtest.hpp>
+#include <stout/hashset.hpp>
 #include <stout/os.hpp>
 
 #include "slave/containerizer/mesos/provisioner/paths.hpp"
@@ -77,6 +78,50 @@ TEST_F(ProvisionerPathTest, FindContainerDirectory)
   find = findContainerDir(provisionerDir, child4);
   ASSERT_SOME(find);
   EXPECT_NONE(find.get());
+}
+
+
+TEST_F(ProvisionerPathTest, ListProvisionerContainers)
+{
+  const string provisionerDir = os::getcwd();
+
+  // TODO(gilbert): Refactor duplicate code to the unit test
+  // class.
+  ContainerID child1;
+  ContainerID child2;
+  ContainerID child3;
+  ContainerID parent1;
+  ContainerID parent2;
+
+  child1.set_value("child_1_1");
+  child1.mutable_parent()->set_value("parent_1");
+  child2.set_value("child_1_2");
+  child2.mutable_parent()->set_value("parent_1");
+  child3.set_value("child_2_1");
+  child3.mutable_parent()->set_value("parent_2");
+  parent1.set_value("parent_1");
+  parent2.set_value("parent_2");
+
+  const string containerDir1 = getContainerDir(provisionerDir, child1);
+  const string containerDir2 = getContainerDir(provisionerDir, child2);
+  const string containerDir3 = getContainerDir(provisionerDir, child3);
+
+  Try<Nothing> mkdir = os::mkdir(containerDir1);
+  ASSERT_SOME(mkdir);
+  mkdir = os::mkdir(containerDir2);
+  ASSERT_SOME(mkdir);
+  mkdir = os::mkdir(containerDir3);
+  ASSERT_SOME(mkdir);
+
+  Try<hashset<ContainerID>> containerIds = listContainers(provisionerDir);
+  ASSERT_SOME(containerIds);
+
+  EXPECT_TRUE(containerIds->contains(parent1));
+  EXPECT_TRUE(containerIds->contains(parent2));
+  EXPECT_TRUE(containerIds->contains(child1));
+  EXPECT_TRUE(containerIds->contains(child2));
+  EXPECT_TRUE(containerIds->contains(child3));
+  EXPECT_EQ(5u, containerIds->size());
 }
 
 } // namespace tests {
