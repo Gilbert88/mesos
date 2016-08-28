2bb3c687b6952c79495eeed8e054dcd34f3b6d79
Gilbert Song <songzihao1990@gmail.com>
2016-08-27 21:57:12 -0700

    Fixed listcontainer test.

--- src/tests/containerizer/provisioner_paths_tests.cpp
+++ src/tests/containerizer/provisioner_paths_tests.cpp
@@ -87,31 +87,28 @@ TEST_F(ProvisionerPathTest, ListProvisionerContainers)
 
   // TODO(gilbert): Refactor duplicate code to the unit test
   // class.
-  ContainerID child1;
-  ContainerID child2;
-  ContainerID child3;
-  ContainerID parent1;
-  ContainerID parent2;
+  ContainerID child1;  // parent1/child1
+  ContainerID child2;  // parent1/child2
+  ContainerID child3;  // parent2/child3
+  ContainerID parent1; // parent1
+  ContainerID parent2; // parent2
 
-  child1.set_value("child_1_1");
-  child1.mutable_parent()->set_value("parent_1");
-  child2.set_value("child_1_2");
-  child2.mutable_parent()->set_value("parent_1");
-  child3.set_value("child_2_1");
-  child3.mutable_parent()->set_value("parent_2");
-  parent1.set_value("parent_1");
-  parent2.set_value("parent_2");
+  child1.set_value("child1");
+  child1.mutable_parent()->set_value("parent1");
+  child2.set_value("child2");
+  child2.mutable_parent()->set_value("parent1");
+  child3.set_value("child3");
+  child3.mutable_parent()->set_value("parent2");
+  parent1.set_value("parent1");
+  parent2.set_value("parent2");
 
   const string containerDir1 = getContainerDir(provisionerDir, child1);
   const string containerDir2 = getContainerDir(provisionerDir, child2);
   const string containerDir3 = getContainerDir(provisionerDir, child3);
 
-  Try<Nothing> mkdir = os::mkdir(containerDir1);
-  ASSERT_SOME(mkdir);
-  mkdir = os::mkdir(containerDir2);
-  ASSERT_SOME(mkdir);
-  mkdir = os::mkdir(containerDir3);
-  ASSERT_SOME(mkdir);
+  ASSERT_SOME(os::mkdir(containerDir1));
+  ASSERT_SOME(os::mkdir(containerDir2));
+  ASSERT_SOME(os::mkdir(containerDir3));
 
   Try<hashset<ContainerID>> containerIds = listContainers(provisionerDir);
   ASSERT_SOME(containerIds);
