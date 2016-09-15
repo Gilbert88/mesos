21469f8684b678e16844c19c19a11f75f924ff20
Gilbert Song <songzihao1990@gmail.com>
2016-09-14 17:57:17 -0700

    Update mesos containerizer isolate for nested support.
    Review: https://reviews.apache.org/r/51673

--- src/slave/containerizer/mesos/containerizer.cpp
+++ src/slave/containerizer/mesos/containerizer.cpp
@@ -1475,8 +1475,6 @@ Future<bool> MesosContainerizerProcess::isolate(
     const ContainerID& containerId,
     pid_t _pid)
 {
-  CHECK(!containerId.has_parent());
-
   if (!containers_.contains(containerId)) {
     return Failure("Container destroyed during preparing");
   }
