b95c6273e1676f36d8e234ff67ae1e1756ec7e86
Gilbert Song <songzihao1990@gmail.com>
2016-09-21 09:14:35 -0700

    Added containerizer helper method 'getContainerIds()'.
    This is a helper function to collect all containerIds (include top
    level and nested) from the runtime directory.
    
    Review: https://reviews.apache.org/r/52234

--- src/slave/containerizer/mesos/paths.cpp
+++ src/slave/containerizer/mesos/paths.cpp
@@ -14,12 +14,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <stout/lambda.hpp>
 #include <stout/os.hpp>
 #include <stout/path.hpp>
 
 #include "slave/containerizer/mesos/paths.hpp"
 
+using std::list;
 using std::string;
+using std::vector;
 
 namespace mesos {
 namespace internal {
@@ -113,6 +116,93 @@ Result<int> getContainerStatus(
   }
 
   return None();
+}
+
+
+Result<vector<ContainerID>> getContainerIds(const string& runtimeDir)
+{
+  lambda::function<Result<vector<ContainerID>>(
+      const string&,
+      const string&)> helper;
+
+  helper = [&helper](
+      const string& runtimeDir,
+      const string& directory) -> Result<vector<ContainerID>> {
+    // Loop through each container at the path, if it exists.
+    const string path = path::join(runtimeDir, directory);
+
+    if (!os::exists(path)) {
+      return None();
+    }
+
+    Try<list<string>> entries = os::ls(path);
+    if (entries.isError()) {
+      return Error("Failed to list '" + path + "': " + entries.error());
+    }
+
+    // The order always guarantee that a parent container is inserted
+    // before its child containers. This is necessary for constructing
+    // the hashmap 'containers_' in 'Containerizer::recover()'.
+    vector<ContainerID> containers;
+
+    foreach (const string& entry, entries.get()) {
+      // We're not expecting anything else but directories here
+      // representing each container.
+      CHECK(os::stat::isdir(path::join(path, entry)));
+
+      // TODO(benh): Validate that the entry looks like a ContainerID?
+      ContainerID container;
+
+      // Determine the ContainerID from 'directory/entry' (we explicitly
+      // do not want to use `path` because it contains things that we don't
+      // want in our ContainerID and even still we have to skip all
+      // instances of 'containers' as well).
+      vector<string> tokens =
+        strings::tokenize(
+            path::join(directory, entry),
+            stringify(os::PATH_SEPARATOR));
+
+      foreach (const string& token, tokens) {
+        // Skip the directory separator 'containers'.
+        if (token == CONTAINER_DIRECTORY) {
+          continue;
+        }
+
+        ContainerID id;
+        id.set_value(token);
+
+        if (container.has_value()) {
+          id.mutable_parent()->CopyFrom(container);
+        }
+
+        container = id;
+      }
+
+      // Validate the ID (there should be at least one level).
+      if (!container.has_value()) {
+        return Error(
+            "Failed to determine ContainerID from path '" + path + "'");
+      }
+
+      containers.push_back(container);
+
+      // Now recursively build the list of nested containers.
+      Result<vector<ContainerID>> _containers = helper(
+          runtimeDir,
+          path::join(directory, entry, CONTAINER_DIRECTORY));
+
+      if (_containers.isError()) {
+        return Error(_containers.error());
+      } else if (_containers.isSome()) {
+        containers.insert(
+            containers.end(), _containers->begin(), _containers->end());
+      }
+    }
+
+    return containers;
+  };
+
+  return helper(runtimeDir, CONTAINER_DIRECTORY);
 }
 
 } // namespace paths {
