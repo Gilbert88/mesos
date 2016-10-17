5454537a5f5e86bd67ef510bbae3690f7b24dbba
Gilbert Song <songzihao1990@gmail.com>
2016-10-17 13:47:08 -0700

    JFDIO.

--- src/tests/containerizer/docker_spec_tests.cpp
+++ src/tests/containerizer/docker_spec_tests.cpp
@@ -257,8 +257,7 @@ TEST_F(DockerSpecTest, DecodeHttpResponses)
 
   output =
 "HTTP/1.1 200 Connection established\n"
-"Content-Length: 147\n\n"
-"{\"errors\":[{\"code\":\"UNAUTHORIZED\",\"message\":\"authentication required\",\"detail\":[{\"Type\":\"repository\",\"Name\":\"mesosphere/inky\",\"Action\":\"pull\"}]}]}\n"; // NOLINT(whitespace/line_length)
+"Content-Length: 147\n\n";
 
   // Decode HTTP responses.
   responses =
