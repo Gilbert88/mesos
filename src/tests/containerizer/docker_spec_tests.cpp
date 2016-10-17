1b5f4d5992555ddd53b178ad190ece832a7e23a0
Gilbert Song <songzihao1990@gmail.com>
2016-10-17 15:18:34 -0700

    FDJIo.

--- src/tests/containerizer/docker_spec_tests.cpp
+++ src/tests/containerizer/docker_spec_tests.cpp
@@ -258,7 +258,7 @@ TEST_F(DockerSpecTest, DecodeHttpResponses)
   output =
 "HTTP/1.1 200 Connection established\n"
 "Content-Length: 147\n\n"
-"{}";
+"{}\n";
 
   // Decode HTTP responses.
   responses =
