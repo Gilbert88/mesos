82c9686d4ebd8e243a3f5ada156a266b4e80acc0
Gilbert Song <songzihao1990@gmail.com>
2016-10-17 10:49:37 -0700

    FDJOI.

--- src/tests/containerizer/docker_spec_tests.cpp
+++ src/tests/containerizer/docker_spec_tests.cpp
@@ -262,7 +262,7 @@ TEST_F(DockerSpecTest, DecodeHttpResponses)
 "Www-Authenticate: Bearer realm=\"https://auth.docker.io/token\",service=\"registry.docker.io\",scope=\"repository:mesosphere/inky:pull\"\n" // NOLINT(whitespace/line_length)
 "Date: Fri, 14 Oct 2016 19:04:35 GMT\n"
 "Content-Length: 147\n"
-"Strict-Transport-Security: max-age=31536000\n\n";
+"Strict-Transport-Security: max-age=31536000\n\n"
 "{\"errors\":[{\"code\":\"UNAUTHORIZED\",\"message\":\"authentication required\",\"detail\":[{\"Type\":\"repository\",\"Name\":\"mesosphere/inky\",\"Action\":\"pull\"}]}]}\n"; // NOLINT(whitespace/line_length)
 
   // Decode HTTP responses.
