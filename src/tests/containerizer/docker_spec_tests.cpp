d1ae54c72caeaef6ed8a539f1520065573f75dbe
Gilbert Song <songzihao1990@gmail.com>
2016-10-17 10:12:26 -0700

    FJDIO.

--- src/tests/containerizer/docker_spec_tests.cpp
+++ src/tests/containerizer/docker_spec_tests.cpp
@@ -257,6 +257,11 @@ TEST_F(DockerSpecTest, DecodeHttpResponses)
 
   output =
 "HTTP/1.1 200 Connection established\n"
+"Content-Type: application/json; charset=utf-8\n"
+"Docker-Distribution-Api-Version: registry/2.0\n"
+"Www-Authenticate: Bearer realm=\"https://auth.docker.io/token\",service=\"registry.docker.io\",scope=\"repository:mesosphere/inky:pull\"\n" // NOLINT(whitespace/line_length)
+"Date: Fri, 14 Oct 2016 19:04:35 GMT\n"
+"Content-Length: 147\n"
 "{\"errors\":[{\"code\":\"UNAUTHORIZED\",\"message\":\"authentication required\",\"detail\":[{\"Type\":\"repository\",\"Name\":\"mesosphere/inky\",\"Action\":\"pull\"}]}]}\n" // NOLINT(whitespace/line_length)
 "\nHTTP/1.1 401 Unauthorized\n"
 "Content-Type: application/json; charset=utf-8\n"
